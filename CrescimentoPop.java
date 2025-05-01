d62f8153753340fb8dc196ae46e761df952a2935
thgomes3 <thiagovg9@icloud.com>
2025-04-30 23:32:36 -0300

    Update and rename CrescimentoPopulacional to CrescimentoPop.java

--- CrescimentoPop.java
+++ CrescimentoPop.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CrescimentoPopulacional {
+public class CrescimentoPop { 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int populacaoA, populacaoB, anos;
